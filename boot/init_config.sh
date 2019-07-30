8beea7a5495596600b722f5fbfc7c1d30b6563a1
mksteveuk <steve@hearnden.org.uk>
2019-07-30 10:32:28 +0100

    First attempt at functional usbgadget - removed all payloads - taken out reconfigures - delivering usb functionality only

--- boot/init_config.sh
+++ boot/init_config.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 
-#    This file is part of P4wnP1.
+#    This file is part of usbgadget (from P4wnP1).
 #
-#    Copyright (c) 2017, Marcus Mengs. 
+#    Copyright (c) 2017, Marcus Mengs. Amended Steve Hearnden 2019
+#
+#    usbgadget is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    usbgadget is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
 #    P4wnP1 is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -24,13 +34,5 @@
 # include setup.cfg
 source $wdir/setup.cfg
 
-# include payload (overrides variables set by setup.cfg if needed)
-# PAYLOAD itself is define in setup.cfg
-source $wdir/payloads/$PAYLOAD
-
 # check for wifi capability
 if $wdir/wifi/check_wifi.sh; then WIFI=true; else WIFI=false; fi
-
-# set variable for USB gadget directory
-GADGETS_DIR="mame82gadget"
-
